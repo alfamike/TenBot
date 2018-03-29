cb2a48f0e3fa6d1e3637267049a77e3d1874cd6b
Alvaro <Alvaro@Alvaro-PC>
2018-03-30 00:53:52 +0200

    Prueba de la creación de html

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 '''
 Created on 28 mar. 2018
 
@@ -12,11 +13,22 @@ bot= telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     cid= message.chat.id
-    start_message= "Bienvenido al bot de Gestión del Grupo 10"
+    start_message= "Bienvenido al bot de GestiÃ³n del Grupo 10"
     bot.send_message(cid, start_message)
+
+@bot.message_handler(commands=['stats'], content_types=['text'])
+def stats_handler(message):
+    f= open('../tmp/prueba.html','w')
+    pagina= '''<!DOCTYPE HTML><html><head><title>Resultados</title></head><body><h1>
+    Hola mundo</h1><p>Cuerpo</p></body></html>'''
+    f.write(pagina)
+    f.close()
+
+    f= open('../tmp/prueba.html','r')
+    cid= message.chat.id
+    bot.send_document(cid,f)
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-
 bot.polling()
