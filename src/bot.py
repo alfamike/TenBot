407f2bf015a72967774728bc3f8f63f22c9e5de3
alfamike <amenachor@gmail.com>
2018-04-18 20:31:41 +0200

    prueba autor commit

--- src/bot.py
+++ src/bot.py
@@ -161,62 +161,6 @@ def fdb_handler(message):
     cid= message.chat.id
     bot.send_document(cid,fdb)
     
-@bot.message_handler(commands=['stats'], content_types=['text'])
-def stats_handler(message):
-    f= open('../tmp/prueba.html','w')
-    pagina= '''<!DOCTYPE HTML><html>
-  <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-
-  <body>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
-  </body>
-</html>'''
-    f.write(pagina)
-    f.close()
-
-    f= open('../tmp/prueba.html','r')
-    cid= message.chat.id
-    bot.send_document(cid,f)
-    
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
